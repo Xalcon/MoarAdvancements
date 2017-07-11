e06730d77761de9bc7f9326d757fe509fbcf10ae
xalcon <xalcon@xalcon.net>
2017-07-11 14:40:56 +0200

    set acceptable remote version to *, just in case o.o"

--- src/main/java/net/xalcon/moaradvancements/MoarAdvancements.java
+++ src/main/java/net/xalcon/moaradvancements/MoarAdvancements.java
@@ -2,5 +2,5 @@ package net.xalcon.moaradvancements;
 
         import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "moaradvancements", version = "@VERSION@", certificateFingerprint = "@CERT_FINGERPRINT@", clientSideOnly = true)
+@Mod(modid = "moaradvancements", version = "@VERSION@", certificateFingerprint = "@CERT_FINGERPRINT@", clientSideOnly = true, acceptableRemoteVersions = "*")
 public class MoarAdvancements { }
