f305e2e352732c24db5313c7aac93452a32814ce
xalcon <xalcon@xalcon.net>
2017-07-11 14:29:40 +0200

    configured jar signing and curse deployment (i think)

---
+++ changelog.md
@@ -0,0 +1,3 @@
+### v1.0
+* **First Release**
+* Adds 2 Buttons to the Advancements Menu for paging (26 tabs per page)
--- src/main/java/net/xalcon/moaradvancements/MoarAdvancements.java
+++ src/main/java/net/xalcon/moaradvancements/MoarAdvancements.java
@@ -2,5 +2,5 @@ package net.xalcon.moaradvancements;
 
         import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "moaradvancements", version = "@VERSION@")
+@Mod(modid = "moaradvancements", version = "@VERSION@", certificateFingerprint = "@CERT_FINGERPRINT@")
 public class MoarAdvancements { }
