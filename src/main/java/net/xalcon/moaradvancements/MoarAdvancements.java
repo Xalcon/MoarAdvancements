ea8e7c06fc5573c1743c34d6e634e2c51ab39f2d
Xalcon <xalcon@xalcon.net>
2017-08-01 22:00:02 +0200

    mod merged into forge, limit usage to <14.21.1.2436

--- src/main/java/net/xalcon/moaradvancements/MoarAdvancements.java
+++ src/main/java/net/xalcon/moaradvancements/MoarAdvancements.java
@@ -2,5 +2,10 @@ package net.xalcon.moaradvancements;
 
         import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "moaradvancements", version = "@VERSION@", certificateFingerprint = "@CERT_FINGERPRINT@", clientSideOnly = true, acceptableRemoteVersions = "*")
+@Mod(modid = "moaradvancements",
+        version = "@VERSION@",
+        certificateFingerprint = "@CERT_FINGERPRINT@",
+        clientSideOnly = true,
+        acceptableRemoteVersions = "*",
+        dependencies = "required-after:forge@[14.21.1.0,14.21.1.2435]")
 public class MoarAdvancements { }
